bf28468b03e8e1f535811859eddcfc3562a70edb
ByeonggakYu <qudrkr0815@korea.ac.kr>
2022-02-09 22:55:25 +0900

    [practice]

--- dist/index.js
+++ dist/index.js
@@ -7,10 +7,47 @@ let c = {
 };
 let d = c.apple + 4;
 console.log(c, d);
-const Minji = {
-    name: "minji",
-    age: 27,
-};
 console.log("타입 별칭");
-console.log(Minji);
+///////////////////////////
+let b3 = undefined;
+let a1 = null;
+let b2 = "apple";
+const c1 = "pinapple";
+let d1 = [true, false, false];
+let e1 = { type: "ficus" };
+let f1 = [1, false];
+const g = [3];
+let h = null;
+function voidFunction() {
+    console.log("dd");
+}
+voidFunction();
+let my = { name: "gak", age: 23 };
+function changeAge(person, num) {
+    person.age += num;
+    return person;
+}
+let ret = changeAge(my, 3);
+console.log(ret);
+//////////////////////////
+function* createFiboGenerater() {
+    let a = 0;
+    let b = 1;
+    while (true) {
+        yield a;
+        [a, b] = [b, a + b];
+    }
+}
+let generator = createFiboGenerater();
+console.log(generator.next()); // {value: 0, done: false};
+console.log(generator.next()); // {value: 1, done: false};
+console.log(generator.next()); // {value: 1, done: false};
+console.log(generator.next()); // {value: 2, done: false};
+let signiture = (firstString, secondString) => {
+    firstString += "a";
+    secondString += "c";
+    return firstString + secondString;
+};
+let rett = signiture("aa", "cc");
+console.log(rett);
 //# sourceMappingURL=index.js.map
